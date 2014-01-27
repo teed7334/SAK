63b7234e689b525838905938df02a88137c1b6a0
Peter <teed7334@gmail.com>
2014-01-26 21:28:40 -0800

    Create Layout Example

--- controller/index.php
+++ controller/index.php
@@ -3,9 +3,19 @@ class index extends Controller {
 	
 	public function action_index() {
 		$this->assign('hello', 'Hello World!!!!');
+		$this->layout2();
+	}
+
+	public function layout1() {
 		$this->element('all.html41.header');
 		$this->view('index');
 		$this->element('all.html41.footer');
 	}
+
+	public function layout2() {
+		$this->element('all.html5.header');
+		$this->view('index');
+		$this->element('all.html5.footer');
+	}	
 
 }
